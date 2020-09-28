169cad5f823737bdfb1efb356cefd8173f3df760
endieposts <13526487+endieposts@users.noreply.github.com>
2020-09-28 21:45:50 +0100

    Experiments with influx

--- src/main/java/com/endie/avizandum/gameplay/controller/WebController.java
+++ src/main/java/com/endie/avizandum/gameplay/controller/WebController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 
 @Controller
 public class WebController {
@@ -20,9 +22,22 @@ public class WebController {
         return "player/playerhome";
     }
 
+    @GetMapping(path = "/countme")
+    public String countMe(HttpServletRequest request) throws ServletException {
+        String user = "test"; // request.getRemoteUser();
+        increaseCount(user);
+        return "welcome";
+    }
+
     @GetMapping(path = "/logout")
     public String logout(HttpServletRequest request) throws ServletException {
+        String user = request.getRemoteUser();
         request.logout();
         return "welcome";
+    }
+
+    private void increaseCount(String user) {
+        Counter counter = Metrics.counter("request.countMe", "User", user);
+        counter.increment();
     }
 }
