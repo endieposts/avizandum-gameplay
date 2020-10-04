dc37882a2d371319e2c3997505ebe03be742f428
endieposts <13526487+endieposts@users.noreply.github.com>
2020-10-04 21:02:16 +0100

    WebController tests passing

--- src/test/java/com/endie/avizandum/gameplay/controller/SmokeTest.java
+++ src/test/java/com/endie/avizandum/gameplay/controller/SmokeTest.java
@@ -3,10 +3,12 @@ package com.endie.avizandum.gameplay.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes= WebController.class)
+@ActiveProfiles("test")
 public class SmokeTest {
 
     @Autowired
--- src/test/java/com/endie/avizandum/gameplay/controller/WebControllerTest.java
+++ src/test/java/com/endie/avizandum/gameplay/controller/WebControllerTest.java
@@ -1,6 +1,7 @@
 package com.endie.avizandum.gameplay.controller;
 
 import com.c4_soft.springaddons.security.oauth2.test.annotations.keycloak.WithMockKeycloakAuth;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
@@ -10,16 +11,20 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 
 @AutoConfigureMockMvc
@@ -32,13 +37,25 @@ public class WebControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @WithMockKeycloakAuth("player")
-    public void greetingShouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("http://localhost:8083/")).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content().string(containsString("Hello")));
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
+    @WithMockUser(username = "admin", roles = {"player", "admin"})
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("http://localhost:8080/")).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content().string(containsString("Hello")));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"player", "admin"})
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/test")).andDo(print()).andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(containsString("Hello, World")));
