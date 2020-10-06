e6c98b06fb1a5839a5e401c2f776ea805fe9d057
endieposts <13526487+endieposts@users.noreply.github.com>
2020-10-06 10:17:18 +0100

    Add domain repo test

---
+++ src/test/java/com/endie/avizandum/gameplay/repository/world/DomainRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.endie.avizandum.gameplay.repository.world;
+
+import com.endie.avizandum.gameplay.model.world.Domain;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class DomainRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    DomainRepository domainRepository;
+
+    @Test
+    public void should_find_no_domains_if_repository_is_empty() {
+        Iterable<Domain> domains = domainRepository.findAll();
+
+        assertThat(domains).isEmpty();
+    }
+}
